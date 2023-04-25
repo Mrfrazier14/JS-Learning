d42d666d272c20cbbdbba4660acb66a8e1dd6a3d
Aaron Frazier Jr <afraz91@wgu.edu>
2023-04-24 18:29:30 -0700

    Update main.js

--- main.js
+++ main.js
@@ -4,3 +4,9 @@ console.log(-4 * 1.5); // => -6
 console.log(25 / 8); // => 3.125
 
 // I will be adding more math to these lines
+
+console.log(10 % 3); // => 1
+console.log(14 % 5); // => 4
+console.log(20 % 17); // => 3
+console.log(18 % 6); // => 0
+console.log(7 % 9); // => 7
